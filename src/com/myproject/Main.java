e7b22797e4a74cce74e40132abd94e9dc4cd94db
Karthikeyan_M <karthikeyan.m@khoros.com>
2021-09-17 07:47:24 +0530

    Twitter Project Java Version

---
+++ src/com/myproject/Main.java
@@ -0,0 +1,58 @@
+package com.myproject;
+
+
+import twitter4j.Twitter;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        Twitter twitter = TwitterObjectBuilder.getInstance();
+        Scanner scanner = new Scanner(System.in);
+
+        int choice;
+        boolean run=true;
+
+        System.out.println("-----------------------------------------Welcome to Tweet@Java------------------------------------------\n");
+
+        while(run) {
+            System.out.print("\n-------------------Options-------------------\n" +
+                    "1. Post a Tweet\n" +
+                    "2. Watch TimeLine\n" +
+                    "3. Exit\n\n" +
+                    "Enter your Choice : ");
+            choice = scanner.nextInt();
+
+            switch(choice)
+            {
+                case 1:
+                {
+                    TweetPoster.postTweet(twitter);
+                    break;
+                }
+
+                case 2:
+                {
+                    HomeLineViewer.viewHomeLine(twitter);
+                    break;
+                }
+
+                case 3:
+                {
+                    run=false;
+                    System.out.println(" --- THANK YOU --- ");
+                    break;
+                }
+
+                default:
+                {
+                    System.out.println("Please Enter Proper Input !!!\n");
+                }
+            }
+        }
+
+    }
+}
